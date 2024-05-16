a11feb1fde477782a864b65189ba3de29142e612
GCodec89 <g.o.s.codeco@gmail.com>
2024-05-16 21:40:26 +0100

    gnl-42

---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gonolive <gonolive@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 21:31:22 by gonolive          #+#    #+#             */
+/*   Updated: 2024/05/16 21:32:01 by gonolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# define MAX_FD 1024
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *txt, char *buff);
+int		ft_strlen(char *str);
+char	*ft_strchr(char *txt, int c);
+
+#endif
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gonolive <gonolive@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/11 17:38:24 by gonolive          #+#    #+#             */
-/*   Updated: 2024/05/15 06:16:51 by gonolive         ###   ########.fr       */
+/*   Created: 2024/05/16 21:32:25 by gonolive          #+#    #+#             */
+/*   Updated: 2024/05/16 21:32:52 by gonolive         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*ft_strchr(char *txt, int c)
 {
